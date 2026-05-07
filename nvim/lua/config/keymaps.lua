a723ca968790f6a9dae261e8ff3547c31749aad4
ozzo <ozzo@nuc.local>
2026-05-07 19:06:29 +0200

    feat(nvim): add terminal escape and buffer navigation keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+
+-- Exit terminal mode with Esc (easier than Ctrl+\ Ctrl+N)
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- Navigate buffers with Tab / Shift+Tab
+vim.keymap.set("n", "<Tab>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+vim.keymap.set("n", "<S-Tab>", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
+
+-- Open a terminal in a new buffer
+vim.keymap.set("n", "<leader>tt", "<cmd>terminal<cr>", { desc = "Open terminal" })
