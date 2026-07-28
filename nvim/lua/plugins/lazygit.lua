bda683b9a2c6de2cfe0839bb9f8c3e882d816d33
ozzo <ozzo@nuc.local>
2026-07-28 14:30:54 +0200

    iterm url config and lazygit plugin update for prs

--- nvim/lua/plugins/lazygit.lua
+++ nvim/lua/plugins/lazygit.lua
@@ -1,6 +1,17 @@
 return {
   {
     "folke/snacks.nvim",
+    opts = {
+      lazygit = {
+        config = {
+          os = {
+            -- Send URLs over SSH to iTerm2 on the Mac instead of asking the
+            -- headless NUC to open a local browser with xdg-open.
+            openLink = "iterm-open-url {{link}}",
+          },
+        },
+      },
+    },
     keys = {
       {
         "<leader>gg",
